59c9865220e960ab3605c08938f20816a32ea2fb
MrArtemSid <artem.sid.key@gmail.com>
2024-04-02 23:55:15 +0400

    OS: Lab4: Import list realization from Lab2

---
+++ Lab4/list.h
@@ -0,0 +1,13 @@
+/**
+ * list data structure containing the tasks in the system
+ */
+
+struct node {
+    Task *task;
+    struct node *next;
+};
+
+// insert and delete operations.
+void insert(struct node **head, Task *task);
+void delete(struct node **head, Task *task);
+void traverse(struct node *head);

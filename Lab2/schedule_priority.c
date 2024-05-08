76a24a924f593095f5c50db236e8fc31d562563c
phantom6d <pochtaforamino@gmail.com>
2024-05-08 22:16:04 +0400

    OS: Lab2: added priority scheduler + priority quick sort

---
+++ Lab2/schedule_priority.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "task.h"
+#include "list.h"
+#include "cpu.h"
+#include "schedulers.h"
+#include "quick_sort_priority.h"
+
+int task_cnt = 0;
+Task arr[100];
+
+void add(char *name, int priority, int burst) {
+    arr[task_cnt].name = name;
+    arr[task_cnt].priority = priority;
+    arr[task_cnt].burst = burst;
+    task_cnt += 1;
+}
+
+
+void schedule() {
+    quick_sort(arr, 0, task_cnt - 1);
+    while (task_cnt) {
+        --task_cnt;
+        run(&arr[task_cnt], arr[task_cnt].burst);
+    }
+}

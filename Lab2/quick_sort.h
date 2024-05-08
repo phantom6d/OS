d6fa22071ac7c8085d35a4685175be393feb8221
phantom6d <pochtaforamino@gmail.com>
2024-05-08 21:19:59 +0400

    OS: Lab2: added sjf scheduler + quicksort for sjf

---
+++ Lab2/quick_sort.h
@@ -0,0 +1,46 @@
+#ifndef LAB2_QUICK_SORT_H
+#define LAB2_QUICK_SORT_H
+#include "task.h"
+
+void swap(Task* a, Task* b)
+{
+    Task temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition(Task arr[], int left, int right)
+{
+
+    int pivot = arr[left].burst*(-1);
+    int i = left;
+    int j = right;
+
+    while (i < j) {
+
+        while (arr[i].burst*(-1) <= pivot && i <= right - 1) {
+            i++;
+        }
+
+        while (arr[j].burst*(-1) > pivot && j >= left + 1) {
+            j--;
+        }
+        if (i < j) {
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[left], &arr[j]);
+    return j;
+}
+
+void quick_sort(Task arr[], int left, int right)
+{
+    if (left < right) {
+
+        int partition_index = partition(arr, left, right);
+        quick_sort(arr, left, partition_index - 1);
+        quick_sort(arr, partition_index + 1, right);
+    }
+}
+
+#endif //LAB2_QUICK_SORT_H
